6a3185e94dcd78d0856eb94afd7c21b4f558ec0c
Sanjeet Kumar Gupta <sanjeetskg1212@gmail.com>
2025-07-07 11:43:00 +0530

    update all data

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["http://localhost:3000/"],
 		methods: ["GET", "POST"],
 	},
 });
@@ -35,4 +35,4 @@ io.on("connection", (socket) => {
 	});
 });
 
-export default { app, io, server };
+export { app, io, server };
