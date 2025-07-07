ddb4d779d7b5f18bde3472936e9007a64de35312
Sanjeet Kumar Gupta <sanjeetskg1212@gmail.com>
2025-07-07 11:47:01 +0530

    update all data

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 dotenv.config();
 const connectToMongoDB = async () => {
 	try {
-		console.log("MONGO_DB_URI:", process.env.MONGO_DB_URI);
+		
 		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
