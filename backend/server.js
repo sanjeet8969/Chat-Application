c1949194ddc963e5b17f6457a5f600b6fe4cb000
Sanjeet Kumar Gupta <sanjeetskg1212@gmail.com>
2025-07-07 12:26:53 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+//import cors from "cors"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app,server} from "./socket/socket.js"
 
@@ -19,12 +19,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-const corsOption = {
-  origin: "https://chat-application-1-93f9.onrender.com/",
-  origin: "http://localhost:3000",
-  credentials: true,
-};
-app.use(cors(corsOption));
+// const corsOption = {
+//   origin: "https://chat-application-1-93f9.onrender.com/",
+//   origin: "http://localhost:3000",
+//   credentials: true,
+// };
+// app.use(cors(corsOption));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
